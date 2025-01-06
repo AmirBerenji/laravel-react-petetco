0ea251053461fd0d8a0e9a55ce2f0336bfcfdf22
Navid Safavi <navidsafavico@gmail.com>
2025-01-06 23:17:39 +0400

    fix: interface declration

--- app/Interfaces/IClinicRepository.php
+++ app/Interfaces/IClinicRepository.php
@@ -3,6 +3,7 @@
 namespace App\Interfaces;
 
 use App\DTO\ClinicBranchAddDto;
+use App\Models\Clinic;
 use App\Models\User;
 use Illuminate\Support\Collection;
 
@@ -10,5 +11,5 @@ interface IClinicRepository
 {
     public function getByUser(User $user): Collection;
 
-    public function addClinic(ClinicBranchAddDto $clinic): Collection;
+    public function addClinic(ClinicBranchAddDto $clinic): Clinic;
 }
